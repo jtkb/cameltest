d7aebcde54df4a5a5a7d51af10ede8dcdc79ab3d
Kerry Billingham <contact@AvionicEngineers.com>
2017-11-11 20:08:35 +0000

    Use Blueprint annotations on Bean classes.

--- simpleservice/src/main/java/com/javatechnics/camel/UserServiceImpl.java
+++ simpleservice/src/main/java/com/javatechnics/camel/UserServiceImpl.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,8 @@
 package com.javatechnics.camel;
 
 import com.javatechnics.camel.service.UserService;
+import org.apache.aries.blueprint.annotation.Bean;
+import org.apache.aries.blueprint.annotation.Service;
 
 import java.util.Collection;
 import java.util.Map;
@@ -25,17 +27,18 @@ import java.util.TreeMap;
 /**
  * A {@link org.apache.camel.example.rest.User} service which we rest enable from the {@link org.apache.camel.example.rest.UserRouteBuilder}.
  */
-/*@Bean(id = "userService")
-@Service(interfaces = {UserService.class})*/
+@Bean(id = "userService")
+@Service(interfaces = {UserService.class})
 public class UserServiceImpl implements UserService
 {
 
     // use a tree map so they become sorted
     private final Map<String, User> users = new TreeMap<String, User>();
 
-    public UserServiceImpl() {
+    public UserServiceImpl()
+    {
         users.put("123", new User(123, "John Doe"));
-        users.put("456", new User(456, "Donald Duck"));
+        users.put("456", new User(456, "K-Dog Kutz"));
     }
 
     /**
@@ -45,7 +48,8 @@ public class UserServiceImpl implements UserService
      * @param id
      */
     @Override
-    public User getUser(final String id) {
+    public User getUser(final String id)
+    {
         return users.get(id);
     }
 
@@ -55,7 +59,8 @@ public class UserServiceImpl implements UserService
      * @return the list of all users
      */
     @Override
-    public Collection<User> listUsers() {
+    public Collection<User> listUsers()
+    {
         return users.values();
     }
 
@@ -65,7 +70,8 @@ public class UserServiceImpl implements UserService
      * @param user the user
      */
     @Override
-    public void updateUser(User user) {
+    public void updateUser(User user)
+    {
         users.put("" + user.getId(), user);
     }
 }
